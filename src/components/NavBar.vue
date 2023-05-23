327fe84859b1e731df2e9e6d1496c1ced8c90835
Daniel <danielgamezgonzalez6@gmail.com>
2023-05-23 21:47:35 +0200

    repair logo src

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
     <div class="container-fluid">
       <a class="navbar-brand">
-        <img src="../assets/Logo.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+        <img src="../assets/logo.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
         Kanten
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
