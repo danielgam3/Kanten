43b5a5e5568cd9c3429118f0ac44a15b35483eb0
Daniel <danielgamezgonzalez6@gmail.com>
2023-05-25 01:25:28 +0200

    Home 80% about to be done

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="container-fluid bg-dark">
+  <div class="container-fluid bg-dark mx-auto">
+    <div class="row">
+      <div class="col-md-12 mx-auto">
+        <div class="d-flex justify-content-evenly text-uppercase fs-5">
+          <p class="m-0">w  e  l  c  o  m  e</p>
+        </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12 mx-auto">
+        <div class="d-flex justify-content-evenly text-uppercase fs-5">
+          <p class="m-0">t o</p>
+        </div>
+      </div>
+    </div>
+  </div>
+    <div class="row">
+      <div class="col-md-12 mx-auto">
+        <div class="d-flex justify-content-evenly text-uppercase fs-5 custom-letter-spacing">
+          <p class="m-0">k</p>
+          <p class="m-0">a</p>
+          <p class="m-0">n</p>
+          <p class="m-0">t</p>
+          <p class="m-0">e</p>
+          <p class="m-0">n</p>
+        </div>
+    </div>
+  </div>
 <div id="carouselExample" class="carousel slide">
   <div class="carousel-inner">
     <div class="carousel-item active">
@@ -8,9 +34,6 @@
     <div class="carousel-item">
       <img src="../assets/img_carousel_main/event2.avif" class="d-block w-100 carousel-img" alt="...">
     </div>
-    <!-- <div class="carousel-item">
-      <img src="..." class="d-block w-100 carousel-img" alt="...">
-    </div> -->
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -22,70 +45,72 @@
   </button>
 </div>
 <div class="row m-auto pt-2">
-    <div class="col-md-8 m-auto p-auto">
-      <div class="card bg-dark" style="width: 100%;">
-        <div id="cardExample" class="carousel slide m-auto p-auto" style="width: 96%" data-bs-ride="carousel" data-bs-interval="1500">
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img src="../assets/img_carousel_event/Event1.jpeg" class="d-block w-100 carousel-img" alt="...">
-            </div>
-            <div class="carousel-item">
-              <img src="../assets/img_carousel_event/Event2.jpeg" class="d-block w-100 carousel-img" alt="...">
-            </div>
-            <div class="carousel-item">
-              <img src="../assets/img_carousel_event/Event3.jpeg" class="d-block w-100 carousel-img" alt="...">
-            </div>
-            <div class="carousel-item">
-              <img src="../assets/img_carousel_event/Event4.jpeg" class="d-block w-100 carousel-img" alt="...">
-            </div>
-            <div class="carousel-item">
-              <img src="../assets/img_carousel_event/Event5.jpeg" class="d-block w-100 carousel-img" alt="...">
-            </div>
-          </div>
+  <div class="col-md-6">
+    <div class="card bg-dark" style="width: 100%;">
+      <div class="card-body text-bg-dark rounded-3">
+        <h5 class="card-title">About Us!</h5>
+        <p class="card-text">
+          The venue Kanten opens on 12 May on the 1st floor of Huset Esbjerg.
+          It will be Esbjerg's new focal point for the local musical growth layer
+          and the youth. The venue, which primarily deals with the growth layer in
+          underground music, will house everything from art exhibitions and DJ school,
+          to electronic parties and concerts with room for diversity. Esbjerg lacks more venues and cultural initiatives,
+          and here Kanten will be a safe alternative, with a safe space policy that will make Esbjerg's nightlife safer.
+          In the same way, it is our ambition to create an alternative platform for the youth environment in Esbjerg,
+          where the music program is characterized by a versatile selection of subgenres across genders and identities.
+          On the first floor of Huset Esbjerg's premises, we are creating a youth cultural center and cultural community among young people. The venue Kanten is run by Foreningen Kanten,
+          which is a community of young people from Esbjerg and immigrants who, with the opening of Kanten,
+          will create a voluntarily run association with the possibility of membership.
+          All types are welcome and we want to lift the cultural life in Esbjerg through music and art events.
+        </p>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ModalLocation">
+          Location
+        </button>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-6 mx-auto my-auto">
+    <a href="https://www.tuborgfondet.dk"  target="_blank"> <img class="rounded-3" src="../assets/sponsor1.jpeg" id="sponsor"> </a>
+  </div>
+</div>
+ <div class="row">
+  <div class="col-md-6 mx-auto my-auto">
+    <a href="https://www.kuuf.dk"  target="_blank"> <img class="rounded-3" src="../assets/sponsor2.jpeg" id="sponsor"> </a>
+  </div>
+  <div class="col-md-6">
+    <div id="cardEvents" class="carousel slide m-auto p-auto" style="width: 100%" data-bs-ride="carousel">
+      <div class="carousel-inner rounded-3">
+        <div class="carousel-item active">
+          <img src="../assets/img_carousel_event/Event1.jpeg" class="d-block w-100 carousel-img" alt="...">
         </div>
-        <div class="card-body text-bg-dark">
-          <h5 class="card-title">About Us!</h5>
-          <p class="card-text">
-            The venue Kanten opens on 12 May on the 1st floor of Huset Esbjerg.
-            It will be Esbjerg's new focal point for the local musical growth layer
-            and the youth. The venue, which primarily deals with the growth layer in
-            underground music, will house everything from art exhibitions and DJ school,
-            to electronic parties and concerts with room for diversity. Esbjerg lacks more venues and cultural initiatives,
-            and here Kanten will be a safe alternative, with a safe space policy that will make Esbjerg's nightlife safer.
-            In the same way, it is our ambition to create an alternative platform for the youth environment in Esbjerg,
-            where the music program is characterized by a versatile selection of subgenres across genders and identities.
-            On the first floor of Huset Esbjerg's premises, we are creating a youth cultural center and cultural community among young people. The venue Kanten is run by Foreningen Kanten,
-            which is a community of young people from Esbjerg and immigrants who, with the opening of Kanten,
-            will create a voluntarily run association with the possibility of membership.
-            All types are welcome and we want to lift the cultural life in Esbjerg through music and art events.
-          </p>
-          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ModalLocation">
-            Location
-          </button>
+        <div class="carousel-item">
+          <img src="../assets/img_carousel_event/Event2.jpeg" class="d-block w-100 carousel-img" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="../assets/img_carousel_event/Event3.jpeg" class="d-block w-100 carousel-img" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="../assets/img_carousel_event/Event4.jpeg" class="d-block w-100 carousel-img" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="../assets/img_carousel_event/Event5.jpeg" class="d-block w-100 carousel-img" alt="...">
         </div>
       </div>
-      
-</div>
-  <div class="row m-auto pt-2">
-    <div class="col-md-8 m-auto p-auto">
-      <div class="card bg-dark" style="width: 100%;">
-        <div id="cardExample" class="carousel slide m-auto p-auto" style="width: 96%" data-bs-ride="carousel" data-bs-interval="1500">
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img src="../assets/img_carousel_record/record1.jpg" class="d-block w-100 carousel-img" alt="...">
-            </div>
-            <div class="carousel-item">
-              <img src="../assets/img_carousel_record/record2.jpg" class="d-block w-100 carousel-img" alt="...">
-            </div>
-            <div class="carousel-item">
-              <img src="../assets/img_carousel_record/record3.jpg" class="d-block w-100 carousel-img" alt="...">
-            </div>
-            <div class="carousel-item">
-              <img src="../assets/img_carousel_record/record4.jpg" class="d-block w-100 carousel-img" alt="...">
-            </div>
-          </div>
-        </div>
-        <div class="card-body text-bg-dark">
+      <button class="carousel-control-prev" type="button" data-bs-target="#cardEvents" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">P</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#cardEvents" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">N</span>
+      </button>
+    </div>
+  </div>
+ </div>
+  <div class="row">
+    <div class="col-md-6 mx-auto my-auto">
+      <div class="card bg-dark rounded-3" style="width: 100%;">
+        <div class="card-body text-bg-dark rounded-3">
           <h5 class="card-title">About our new Record Room!</h5>
           <p>
             For several years, Esbjerg has lacked regular rehearsal rooms with the possibility of recording,
@@ -102,21 +127,57 @@
           </button>
         </div>
       </div>
+    </div>
+    <div class="col-md-6 mx-auto my-auto">
+     <a href="https://www.sparnord.dk"  target="_blank"> <img class="rounded-3" src="../assets/sponsor3.png" id="sponsor3"></a>
+    </div>
+    <div class="row">
+      <div class="col-md-6 mx-auto my-auto">
+        <a href="https://lauritzenfonden.com"  target="_blank"> <img class="rounded-3" src="../assets/sponsor4.jpeg" id="sponsor2"> </a>
+      </div>
+      <div class="col-md-6 mx-auto my-auto pb-2">
+        <div id="cardRecordRoom" class="carousel slide m-auto p-auto" style="width: 100%" data-bs-ride="carousel">
+          <div class="carousel-inner rounded-3">
+            <div class="carousel-item active">
+              <img src="../assets/img_carousel_record/record2.jpg" class="d-block w-100 carousel-img" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="../assets/img_carousel_record/record3.jpg" class="d-block w-100 carousel-img" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="../assets/img_carousel_record/record4.jpg" class="d-block w-100 carousel-img" alt="...">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#cardRecordRoom" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">P</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#cardRecordRoom" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">N</span>
+          </button>
+        </div>
+      </div>
+
+    </div>
     
-    </div>
-    </div>
-  </div> 
-  <div class="text-white text-center pt-7 pb-4 w-full lg:w-128 mx-auto grid grid-cols-1 gap-2 place-items-center">
-    <a href="https://www.google.com/maps/place/Gl+Novrupvej+14,+6705+Esbjerg+Kommune/@55.4769721,8.4895869,17z/data=!4m2!3m1!1s0x464b2145d7c77159:0x2bc63adfd5ed6add">
-      <font style="vertical-align: inherit;">
-      <font style="vertical-align: inherit;">GL NOVRUPVEJ 14, 6705 ESBJERG Ø</font>
-    </font>
-  </a>
-  <p>PHONE: 45+ 28 97 11 85</p>
-  <p>EMAIL:
-    <a href="mailto:MUSIKFORENINGENKANTEN@GMAIL.COM">MUSIKFORENINGENKANTEN@GMAIL.COM</a></p>
-    <p><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">COPYRIGHT ©KANTEN 2022</font></font></p>
   </div>
+  <div class="row">
+    <div class="bg-dark">
+            <div class="text-white text-center mx-auto">
+              <a href="https://www.google.com/maps/place/Gl+Novrupvej+14,+6705+Esbjerg+Kommune/@55.4769721,8.4895869,17z/data=!4m2!3m1!1s0x464b2145d7c77159:0x2bc63adfd5ed6add">
+                <font style="vertical-align: inherit;">
+                <font style="vertical-align: inherit;">GL NOVRUPVEJ 14, 6705 ESBJERG Ø</font>
+              </font>
+            </a>
+            <p>PHONE: 45+ 28 97 11 85</p>
+            <p>EMAIL:
+              <a href="mailto:MUSIKFORENINGENKANTEN@GMAIL.COM">MUSIKFORENINGENKANTEN@GMAIL.COM</a></p>
+              <p><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">COPYRIGHT ©KANTEN 2023 By Daniel</font></font></p>
+          </div>
+        </div>
+  </div>
+  </div> 
 
   <div class="modal fade" id="ModalLocation" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
@@ -161,7 +222,6 @@
       </div>
     </div>
   </div>
-</div>
 
 </template>
 
@@ -176,8 +236,24 @@ export default {
 };
 </script>
 <style scoped>
+p{
+  color: white;
+}
 .carousel-img {
   max-height: 500px;
+}
+#sponsor{
+  width: 75%;
+}
+#sponsor2{
+  width: 50%;
+}
+#sponsor3{
+  width: 40%;
+}
+
+.custom-letter-spacing {
+  letter-spacing: -5rem;
 }
 
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-<nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
+  <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
     <div class="container-fluid">
       <a class="navbar-brand">
         <img src="../assets/logo.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
@@ -8,16 +8,43 @@
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="collapse navbar-collapse ms-auto" id="navbarNavAltMarkup">
         <div class="navbar-nav">
           <a class="nav-link" aria-current="page" href="#">Home</a>
           <a class="nav-link" href="#">Events</a>
           <a class="nav-link" href="#">Newsletter</a>
           <a class="nav-link" href="#">Merchandising</a>
         </div>
+        <button type="button" class="btn btn-primary login" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Login</button>
       </div>
     </div>
   </nav>
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email</label>
+            <input type="email" class="form-control mx-auto" style="width:80%;" id="exampleInputEmail1" aria-describedby="emailHelp">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control mx-auto" style="width:80%;" id="exampleInputPassword1">
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Log in</button>
+            <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Sign up!</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+  
 </template>
 
 <script>
@@ -41,6 +68,9 @@ h3 {
   }
   a {
     color: #42b983;
+  }
+  .login{
+    margin-left: auto;
   }
 
 </style>
